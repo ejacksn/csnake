93ae38f7b522d1c19468c57de02339de38439f4a
ejacksn <ejackson1201288@gmail.com>
2025-06-10 15:47:57 -0400

    Head movement implemented: nodelay() call is missing so game is turn-based rn but this is intentional for testing

--- main.c
+++ main.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 #include <ncurses.h>
+#include <stdlib.h>
+
+#include "snake.h"
 
 #define ROWS 11
 #define COLS 25
+
+/* BOARD FUNCTIONS*/
 
 void create_board(char board[ROWS][COLS]){
     int x,y =  0;
@@ -37,19 +42,92 @@ void draw_board(char board[ROWS][COLS]){
 
 }
 
+/*SNAKE FUNCTIONS*/
+
+SnakeNode* create_snake(int y, int x, Direction dir) {
+
+    SnakeNode* snake = malloc(sizeof(SnakeNode));
+    //set struct fields
+    snake->y=y;
+    snake->x=x;
+    snake->dir=dir;
+    return snake;
+}
+
+void update_snake(SnakeNode* head, Direction dir){
+
+    switch(dir) {//swich to modify position  bsed on
+        case UP:
+            head->y--;
+            break;
+        case DOWN:
+            head->y++;
+            break;
+        case LEFT:
+            head->x--;
+            break;
+        case RIGHT:
+            head->x++;
+            break;
+    }
+
+    head->dir=dir; //set direction of head so we can use it for following body segments
+
+}
+
+void mark_snake(char board[ROWS][COLS], SnakeNode* head) {
+    SnakeNode* current = head; //pointer to iterate through snake
+    while (current!=NULL) {
+        board[current->y][current->x] = '0';
+        current = current->next; //move pointer
+    }
+}
+
 
 int main(void) {
 
     char board[ROWS][COLS];   //board is 2D array
+    SnakeNode* snake = create_snake(ROWS/2, COLS/2, LEFT);
+
 
 
 
     initscr();
-    create_board(board);
-    draw_board(board);
-    refresh();
-    getch();
-    endwin();
+    keypad(stdscr, TRUE);   // Enable reading arrow keys
+
+
+    int running = 1;
+    Direction snake_dir = RIGHT;
+
+    while (running) {
+
+        //HANDLE INPUTS
+        int ch = getch();
+        switch (ch) {
+            case KEY_UP:
+                snake_dir=UP; break;
+            case KEY_DOWN:
+                snake_dir=DOWN; break;
+            case KEY_LEFT:
+                snake_dir=LEFT; break;
+            case KEY_RIGHT:
+                snake_dir=RIGHT; break;
+        }
+
+        create_board(board);
+        update_snake(snake, snake_dir);
+        mark_snake(board, snake);
+        draw_board(board);
+
+        napms(100);
+        refresh();
+
+
+
+
+
+    }
+
 
 return 0;
 
