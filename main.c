fb9fabd1c82eab180d282ff77258d472e9c4ecd5
ejacksn <ejackson1201288@gmail.com>
2025-06-10 13:22:45 -0400

    Basic Board Drawing Fucntions implemented: -dreate_board() :handles board memory via 2d char Array
    draw_board(): board to terminal via mvaddch()

--- main.c
+++ main.c
@@ -1,11 +1,56 @@
 #include <stdio.h>
 #include <ncurses.h>
 
+#define ROWS 11
+#define COLS 25
+
+void create_board(char board[ROWS][COLS]){
+    int x,y =  0;
+    for (y=0; y<ROWS; y++){
+
+        for (x=0; x<COLS; x++){
+
+            if (x==0 || x==COLS-1 || y==0 || y==ROWS-1){
+                board[y][x] = 'X';
+            }
+            else{
+                board[y][x] = ' ';
+            }
+}
+
+    }
+
+
+}
+
+void draw_board(char board[ROWS][COLS]){
+    int x,y;
+    for (y=0; y<ROWS; y++){
+
+        for (x=0; x<COLS; x++){
+
+            mvaddch(y,x,board[y][x]);
+}
+
+    }
+
+
+}
+
+
 int main(void) {
+
+    char board[ROWS][COLS];   //board is 2D array
+
+
+
     initscr();
-    printw("\nThis is a test\n   Press any key to exitl.");
+    create_board(board);
+    draw_board(board);
     refresh();
     getch();
     endwin();
+
+return 0;
 
 }
